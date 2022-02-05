07d2b808db465144b57b09a3816a1f43e06dd2e7
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2022-02-05 11:58:34 +0600

    problem fix

--- index.js
+++ index.js
@@ -31,121 +31,174 @@ client.connect(err => {
 
   // Add data to database 
   app.post('/addServices', async (req, res) => {
-    const newServices = req.body
-    await serviceCollection.insertOne(newServices)
-      .then(result => {
-        console.log(result.insertedCount)
-        res.send(result.insertedCount > 0)
-      })
+    try {
+      const newServices = req.body
+      await serviceCollection.insertOne(newServices)
+        .then(result => {
+          console.log(result.insertedCount)
+          res.send(result.insertedCount > 0)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
 
 
   // Add data to UI 
   app.get('/services', async (req, res) => {
-    await serviceCollection.find({}).toArray((err, documents) => {
-      res.send(documents);
-    });
+    try {
+      await serviceCollection.find({}).toArray((err, documents) => {
+        res.send(documents);
+      });
+    } catch (error) {
+      res.send(error.message);
+    }
   });
 
   // Catch Single Data For DAtabase
   app.get('/serviceBook/:id', async (req, res) => {
-    await serviceCollection
-      .find({ _id: ObjectID(req.params.id) })
-      .toArray((err, documents) => {
-        res.send(documents[0]);
-      });
+    try {
+      await serviceCollection
+        .find({ _id: ObjectID(req.params.id) })
+        .toArray((err, documents) => {
+          res.send(documents[0]);
+        });
+    } catch (error) {
+      res.send(error.message);
+      console.log(error.message);
+    }
   });
 
   //single servce load
   app.get('/service/:id', async (req, res) => {
-    await serviceCollection.find({ _id: ObjectID(req.params.id) })
-      .toArray((err, service) => {
-        res.send(service)
-      })
+    try {
+      await serviceCollection.find({ _id: ObjectID(req.params.id) })
+        .toArray((err, service) => {
+          res.send(service)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
   // Update service
   app.patch('/update/:id', async (req, res) => {
-    await serviceCollection.updateOne({ _id: ObjectID(req.params.id) },
-      {
-        $set: { serviceName: req.body.serviceName, price: req.body.price, description: req.body.description, imageURL: req.body.imageURL }
-      })
-      .then(result => {
-        console.log(result);
-        res.send(result)
-      })
+    try {
+      await serviceCollection.updateOne({ _id: ObjectID(req.params.id) },
+        {
+          $set: { serviceName: req.body.serviceName, price: req.body.price, description: req.body.description, imageURL: req.body.imageURL }
+        })
+        .then(result => {
+          console.log(result);
+          res.send(result)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
   // Add DAta OrderCollection
   app.post('/bookOrder', async (req, res) => {
-    const data = req.body;
-    console.log(data);
-    await orderCollection.insertOne({ data }).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
+    try {
+      const data = req.body;
+      console.log(data);
+      await orderCollection.insertOne({ data }).then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+    } catch (error) {
+      res.send(error.message);
+    }
   });
 
   // Get Data From Server 
   app.get('/bookingList', async (req, res) => {
-    await orderCollection.find()
-      .toArray((err, items) => {
-        res.send(items)
-      })
+    try {
+      await orderCollection.find()
+        .toArray((err, items) => {
+          res.send(items)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
 
   // Item Add to database 
 
   app.post('/addReview', async (req, res) => {
-    const newReview = req.body;
-    console.log('adding new review: ', newReview);
-    await reviewCollection.insertOne(newReview)
-      .then(result => {
-        console.log('inserted count', result.insertedCount);
-        res.send(result.insertedCount > 0)
-      })
+    try {
+      const newReview = req.body;
+      console.log('adding new review: ', newReview);
+      await reviewCollection.insertOne(newReview)
+        .then(result => {
+          console.log('inserted count', result.insertedCount);
+          res.send(result.insertedCount > 0)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
   // Get Data From Server 
   app.get('/reviewList', async (req, res) => {
-    await reviewCollection.find()
-      .toArray((err, documents) => {
-        res.send(documents)
-      })
+    try {
+      await reviewCollection.find()
+        .toArray((err, documents) => {
+          res.send(documents)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
 
   // Make Admin 
   app.post('/addAdmin', async (req, res) => {
-    const data = req.body;
-    await adminCollection.insertOne(data).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
+    try {
+      const data = req.body;
+      await adminCollection.insertOne(data).then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+    } catch (error) {
+      res.send(error.message);
+    }
   });
 
   // Admin Filter 
   app.post('/isAdmin', async (req, res) => {
-    const email = req.body.email;
-    await adminCollection.find({ email: email }).toArray((error, documents) => {
-      res.send(documents.length > 0);
-    });
+    try {
+      const email = req.body.email;
+      await adminCollection.find({ email: email }).toArray((error, documents) => {
+        res.send(documents.length > 0);
+      });
+    } catch (error) {
+      res.send(error.message);
+    }
   });
 
 
   app.get('/orderList', async (req, res) => {
-    await orderCollection.find()
-      .toArray((err, items) => {
-        res.send(items)
-      })
+    try {
+      await orderCollection.find()
+        .toArray((err, items) => {
+          res.send(items)
+        })
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
 
   app.delete('/services/:id', async (req, res) => {
-    const id = req.params.id;
-    console.log("ok", id);
-    const serviceDelete = await serviceCollection.deleteOne({ _id: ObjectID(id) })
-    console.log(serviceDelete);
-    res.send(serviceDelete);
+    try {
+      const id = req.params.id;
+      console.log("ok", id);
+      const serviceDelete = await serviceCollection.deleteOne({ _id: ObjectID(id) })
+      console.log(serviceDelete);
+      res.send(serviceDelete);
+    } catch (error) {
+      res.send(error.message);
+    }
   })
 
 
